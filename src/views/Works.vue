67c6494a74a31294cf6b697b8149df22653e0c40
ntaso2051 <normalforce1227@gmail.com>
2020-12-07 15:14:58 +0900

    update

--- src/views/Works.vue
+++ src/views/Works.vue
@@ -38,25 +38,38 @@ export default {
           icon: require("@/assets/ngo.png"),
           description:
             "Nodejs製の対戦型オンラインゲーム。Socket.ioとか使って通信してる。サークルのサーバーで動かしてる。",
-          link: "http://ntasogameonline.n.trap.show/"
+          link: "http://ntasogameonline.n.trap.show/",
         },
         {
           name: ":amazed_fuzzy:",
           icon: require("@/assets/amazed_fuzzy.png"),
           description:
             "同学科の人たちの間で流行ってる顔文字が主人子で〇ミングスみたいなパズルゲーム。難易度はそこそこ。全部使わなくてもクリアできるらしい。",
-          link: "https://ntaso2051.github.io/ntasoGame4/game/"
+          link: "https://ntaso2051.github.io/ntasoGame4/game/",
         },
         {
           name: "A maze",
           icon: require("@/assets/amaze.png"),
           description:
             "穴掘りアルゴリズムを使った迷路ゲーム。実は前作のamazed_fuzzyとa mazeをかけていたりかけていなかったり(激ウマギャグ)。",
-          link: "https://ntaso2051.github.io/ntasoGame5/game/"
-        }
-      ]
+          link: "https://ntaso2051.github.io/ntasoGame5/game/",
+        },
+        {
+          name: "モウモウファイト",
+          icon: require("@/assets/momo.png"),
+          description:
+            "Android用の2vs2のオンライン対戦ゲーム。ブログの方に詳しい遊び方とか書いてあるのでそちらを参照。",
+          link: "https://trap.jp/post/1149/",
+        },
+        {
+          name: "ElGamal暗号を作るやつ",
+          description:
+            "これはゲームではないが、セキュリティキャンプで作成した、文字列をElGamal暗号化するものである。こちらもブログに詳しい説明があるので、ブログのリンクを貼っておく。",
+          link: "https://trap.jp/post/1167/",
+        },
+      ],
     };
-  }
+  },
 };
 </script>
 
